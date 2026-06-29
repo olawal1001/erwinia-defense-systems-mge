e241a996f6f04ff112e266caf8938ffb44058e8c
Rilwan Lawal <153241189+olawal1001@users.noreply.github.com>
2026-06-29 17:32:08 +0200

    Update cluster_identification_VOTUs.R

--- cluster_identification_VOTUs.R
+++ cluster_identification_VOTUs.R
@@ -24,15 +24,15 @@ check1=as.data.frame(cluster1$membership)%>%rownames_to_column(var="Contig")
 clusters_final=check1
 clusters_final$VOTUs=paste0("vOTU", clusters_final$`cluster1$membership`)
 
-cluster_ids=gsub("^.*(GC[AF]_.*)$", "\\1", clusters_final$Contig)
-Genome_ids=gsub("_genomic_virus.*$", "", viral_genome$Genome)
-short_genome_ids=gsub("^(GC[AF]_[0-9]+\\.[0-9]+)_.*$", "\\1", Genome_ids)
-vOTU_Genome=match(cluster_ids, short_genome_ids)
 
-clusters_final$Genome2=Genome_ids[vOTU_Genome]
+clusters_final$Genome=gsub("^.*(GC[AF]_.*)$", "\\1", clusters_final$Contig)
+clusters_final$Genome2=gsub("CheckV.","",
+     gsub("_genomic_virus.*$", "", viral_genome$Genome))
+
 
 vOTU_table=full_join(Quality_table,clusters_final, by = "Genome2")
 
 vOTU_tablee=filter(vOTU_table)%>%dcast(VOTUs+Genome2~., value.var = "Genome2", length)%>%
   select(Viral_Count=".", Genome_Count=".", VOTUs, Genome2)
+vOTU_tablee$VOTUs[is.na(vOTU_tablee$VOTUs)]=0
 vOTU_tablee$VOTUs[is.na(vOTU_tablee$VOTUs)]=0
